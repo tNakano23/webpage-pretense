3a8e769586d4fc225a277dd7ea9a933e0e4ccfcd
tNakano23 <89465605+tNakano23@users.noreply.github.com>
2021-12-24 00:15:16 +0900

    3

--- js2.js
+++ js2.js
@@ -1,21 +1,29 @@
+window.onload = function() {
+    scroll_effect();
 
-$(function(){
     $(window).scroll(function(){
-        $('.eff-fadein').each(function(){
-            let elemPos      = $(this).offset().top;
-            let windowHeight = $(window).height(); 
-            if (scrollY > elemPos - windowHeight + 200){
-                $(this).addClass('eff-scrollin');
-            }
-            // else{
-            //     $(this).removeClass('eff-scrollin');
-            // }
-            if (scrollY > elemPos - windowHeight + 1000){
-                $(this).removeClass('sticky');
-            }
-        });
+    scroll_effect();
     });
-});
+
+    function scroll_effect(){
+        $(window).scroll(function(){
+            $('.eff-fadein').each(function(){
+                let elemPos      = $(this).offset().top;
+                let windowHeight = $(window).height(); 
+                if (scrollY > elemPos - windowHeight + 200){
+                    $(this).addClass('eff-scrollin');
+                }
+                // else{
+                //     $(this).removeClass('eff-scrollin');
+                // }
+                if (scrollY > elemPos - windowHeight + 1000){
+                    $(this).removeClass('sticky');
+                }
+            });
+        });
+    };
+};
+
 
 
 $(function() {
